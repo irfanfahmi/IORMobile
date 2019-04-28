8b87ec3ff5f96a9a84de71c479a7c02c22795718
irfanfahmi24 <irfandoombod@gmail.com>
2019-04-28 21:48:00 +0700

    Add Profil

---
+++ app/src/main/java/com/example/gmf_aeroasia/iormobile/profil_pegawai/ProfilActivity.java
@@ -0,0 +1,15 @@
+package com.example.gmf_aeroasia.iormobile.profil_pegawai;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.gmf_aeroasia.iormobile.R;
+
+public class ProfilActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profil);
+    }
+}

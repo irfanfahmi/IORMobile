e9d371667b27c05c043597557d9b7c4c3895d105
irfanfahmi24 <irfandoombod@gmail.com>
2019-04-30 19:35:05 +0700

    Add Login + servis

--- app/src/main/java/com/example/gmf_aeroasia/iormobile/Login/LoginActivity.java
+++ app/src/main/java/com/example/gmf_aeroasia/iormobile/Login/LoginActivity.java
@@ -1,35 +1,161 @@
 package com.example.gmf_aeroasia.iormobile.Login;
 
+import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.NetworkError;
+import com.android.volley.NoConnectionError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.ServerError;
+import com.android.volley.TimeoutError;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
 import com.example.gmf_aeroasia.iormobile.MainActivity;
+import com.example.gmf_aeroasia.iormobile.MySingleton2;
 import com.example.gmf_aeroasia.iormobile.R;
 import com.example.gmf_aeroasia.iormobile.ReportGuestActivity;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class LoginActivity extends AppCompatActivity {
     private long backPres;
     private Toast backToast;
+    EditText e_user,e_pass;
+    ProgressDialog dialogLoading;
+    SharedPreferences sharedP;
+    SharedPreferences.Editor shareEdit;
+    final String PREF = "Keypref";
+    final String KEY_NAME = "name";
+    final String KEY_ID = "id";
+    final String KEY_UNIT = "unit";
+    final String KEY_USER = "username";
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_activiry);
 
+        e_user = (EditText) findViewById(R.id.e_username);
+        e_pass = (EditText) findViewById(R.id.e_password);
+
         Button btn_login = (Button) findViewById(R.id.btn_login);
         Button btn_login_guest = (Button) findViewById(R.id.btn_login_guest);
+        Log.d("LoginActivity", " user login : " + getprefname());
+
+        if (getprefname().length() != 0) {
+            Intent kliklangsung = new Intent(LoginActivity.this,MainActivity.class);
+            startActivity(kliklangsung);
+        } else {
+
+        }
 
 
         btn_login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), "Login Berhasil", Toast.LENGTH_SHORT).show();
-                Intent i= new Intent(LoginActivity.this,MainActivity.class);
-                startActivity(i);
+                final String user = e_user.getText().toString();
+                final String pass = e_pass.getText().toString();
+
+                Log.d("LoginActivity", " user : " + user.toString());
+                Log.d("LoginActivity", " pass : " + pass.toString());
+                if (!validasi()) {
+                    Toast.makeText(getApplicationContext(), "Login Gagal", Toast.LENGTH_SHORT).show();
+
+
+                } else {
+                    dialogLoading = ProgressDialog.show(LoginActivity.this, "",
+                            "Loading. Please wait...", true);
+
+
+                    String url = "http://"+getApplicationContext().getString(R.string.ip_default)+"/API_IOR/login.php";
+
+                    Log.d("LoginActivity", " URL API : " + url.toString());
+                    StringRequest stringRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+                        @Override
+                        public void onResponse(String response) {
+
+                            if(response.contains("Login Failed")){
+                                Toast.makeText(getApplicationContext(), " Login Gagal, Coba kembali !", Toast.LENGTH_SHORT).show();
+                                dialogLoading.hide();
+                            }else {
+
+                                String data = new String(response);
+                                String[] separated = data.split(",");
+                                shareEdit.putString(KEY_ID, separated[0]);
+                                shareEdit.putString(KEY_NAME, separated[1]);
+                                shareEdit.putString(KEY_UNIT, separated[2]);
+                                shareEdit.putString(KEY_USER, separated[3]);
+                                shareEdit.apply();
+                                startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                                finish();
+                                Toast.makeText(getApplicationContext(), "Selamat Datang "+getprefname().toString(), Toast.LENGTH_SHORT).show();
+//
+
+                                dialogLoading.hide();
+
+                            }
+                        }
+                    }, new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            if (error instanceof TimeoutError || error instanceof NoConnectionError) {
+                                Toast.makeText(getApplicationContext(),
+                                        "Periksa Koneksi Anda dan Silahkan Coba Kembali", Toast.LENGTH_SHORT).show();
+                            } else if (error instanceof ServerError) {
+                                Toast.makeText(getApplicationContext(),
+                                        "Server Mengalami Masalah , Silahkan Coba Kembali", Toast.LENGTH_SHORT).show();
+                            } else if (error instanceof NetworkError) {
+                                Toast.makeText(getApplicationContext(),
+                                        "Jaringan Mengalami Masalah, Silahkan Coba Kembali", Toast.LENGTH_SHORT).show();
+                            } else if (error instanceof AuthFailureError) {
+                                Toast.makeText(getApplicationContext(),
+                                        "Username atau Password Tidak tersedia, Silahkan Coba Kembali", Toast.LENGTH_SHORT).show();
+                            } else {
+                                Toast.makeText(getApplicationContext(),
+                                        "Login Gagal , Silahkan Coba Kembali", Toast.LENGTH_SHORT).show();
+                            }
+                            dialogLoading.hide();
+                            error.printStackTrace();
+                        }
+                    }) {
+                        @Override
+                        protected Map<String, String> getParams() throws AuthFailureError {
+                            HashMap<String, String> data = new HashMap<>();
+                            data.put("username", user);
+                            data.put("password", pass);
+                            return data;
+                        }
+                    };
+                    //MySingleton.getInstance(getApplicationContext()).addToRequestQueue(stringRequest);
+                    stringRequest.setRetryPolicy(new DefaultRetryPolicy(
+                            5000,
+                            DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
+                            DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+                    MySingleton2.getmInstance(getBaseContext()).addToRequestque(stringRequest);
+                }
+
+                sharedP = getSharedPreferences(PREF, Context.MODE_PRIVATE);
+                shareEdit = sharedP.edit();
+
+//                Toast.makeText(getApplicationContext(), "Login Berhasil", Toast.LENGTH_SHORT).show();
+//                Intent i= new Intent(LoginActivity.this,MainActivity.class);
+//                startActivity(i);
             }
         });
 
@@ -43,7 +169,11 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
     }
+    public String getprefname(){
+        sharedP = getSharedPreferences(PREF, Context.MODE_PRIVATE);
+        return sharedP.getString(KEY_NAME,"");
 
+    }
     @Override
     public void onBackPressed() {
 
@@ -57,5 +187,28 @@ public class LoginActivity extends AppCompatActivity {
             backToast.show();
         }
         backPres = System.currentTimeMillis();
+    }
+
+    public boolean validasi() {
+        boolean valid = true;
+
+        final String email = e_user.getText().toString();
+        final String pass = e_pass.getText().toString();
+
+        if (email.isEmpty()) {
+            e_user.setError("Username Harus Terisi");
+            valid = false;
+        } else {
+            e_user.setError(null);
+        }
+
+        if (pass.isEmpty()) {
+            e_pass.setError("Password Harus Terisi");
+            valid = false;
+        } else {
+            e_pass.setError(null);
+        }
+        return valid;
+
     }
 }
--- app/src/main/java/com/example/gmf_aeroasia/iormobile/MainActivity.java
+++ app/src/main/java/com/example/gmf_aeroasia/iormobile/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.gmf_aeroasia.iormobile;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
@@ -13,6 +17,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.gmf_aeroasia.iormobile.Login.LoginActivity;
@@ -24,6 +29,14 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     private Toolbar toolbar;
     private ActionBarDrawerToggle drawerToggle;
     Button btn_IOR_Recived;
+    TextView nama_pegawai,nv_nama,nv_user,nv_unit;
+    SharedPreferences sharedP;
+    SharedPreferences.Editor shareEdit;
+    final String PREF = "Keypref";
+    final String KEY_NAME = "name";
+    final String KEY_ID = "id";
+    final String KEY_UNIT = "unit";
+    final String KEY_USER = "username";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +46,14 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         drawerToggle = setupDrawerToggle();
         toolbar = (Toolbar) findViewById(R.id.toolbarid);
         setSupportActionBar(toolbar);
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav);
+        View headerView = navigationView.getHeaderView(0);
+
+
+        nama_pegawai = (TextView)findViewById(R.id.nama_pegawai);
+        nv_nama = (TextView)headerView.findViewById(R.id.nav_nm_pegawai);
+        nv_user = (TextView)headerView.findViewById(R.id.nav_user_pegawai);
+        nv_unit = (TextView)headerView.findViewById(R.id.nav_unit_pegawai);
         mDrawer.addDrawerListener(drawerToggle);
         drawerToggle.syncState();
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
@@ -46,6 +67,13 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             }
         });
 
+        sharedP =   getSharedPreferences(PREF, Context.MODE_PRIVATE);
+        shareEdit = sharedP.edit();
+
+        nama_pegawai.setText(getprefname().toString());
+        nv_nama.setText(getprefname().toString());
+        nv_user.setText(getprefuser().toString());
+        nv_unit.setText(getprefunit().toString());
 
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -59,6 +87,21 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 //
     }
 
+    public String getprefname(){
+        sharedP = getSharedPreferences(PREF, Context.MODE_PRIVATE);
+        return sharedP.getString(KEY_NAME,"");
+
+    }
+    public String getprefuser(){
+        sharedP = getSharedPreferences(PREF, Context.MODE_PRIVATE);
+        return sharedP.getString(KEY_USER,"");
+
+    }
+    public String getprefunit(){
+        sharedP = getSharedPreferences(PREF, Context.MODE_PRIVATE);
+        return sharedP.getString(KEY_UNIT,"");
+
+    }
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // The action bar home/up action should open or close the drawer.
@@ -96,5 +139,32 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
         return false;
+    }
+
+    public void kliklogout(MenuItem item) {
+
+            AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+            builder.setMessage("Apakah Anda Yakin ?")
+                    .setNegativeButton("No",null)
+                    .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            shareEdit.clear();
+                            shareEdit.commit();
+
+                            // After logout redirect user to Loing Activity
+                            Intent in = new Intent(MainActivity.this, LoginActivity.class);
+                            // Closing all the Activities
+                            in.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+                            // Add new Flag to start new Activity
+                            in.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+                            // Staring Login Activity
+                            MainActivity.this.startActivity(in);
+                        }
+                    });
+            AlertDialog alertDialog = builder.create();
+            alertDialog.show();
     }
 }

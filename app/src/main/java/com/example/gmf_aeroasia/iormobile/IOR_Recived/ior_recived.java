795d783f3d5502930b4f361166fb394b37afdc16
irfanfahmi24 <irfandoombod@gmail.com>
2019-06-16 22:17:24 +0700

    Add Search Ior Recived

--- app/src/main/java/com/example/gmf_aeroasia/iormobile/IOR_Recived/ior_recived.java
+++ app/src/main/java/com/example/gmf_aeroasia/iormobile/IOR_Recived/ior_recived.java
@@ -1,5 +1,7 @@
 package com.example.gmf_aeroasia.iormobile.IOR_Recived;
 
+import android.app.SearchManager;
+import android.content.Context;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.RequiresApi;
@@ -7,9 +9,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
+import android.widget.SearchView;
 import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.DefaultRetryPolicy;
 import com.android.volley.NetworkError;
 import com.android.volley.NoConnectionError;
 import com.android.volley.Request;
@@ -25,10 +33,13 @@ import com.example.gmf_aeroasia.iormobile.service.MySingleton;
 import com.kosalgeek.android.json.JsonConverter;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
-public class ior_recived extends AppCompatActivity {
+public class ior_recived extends AppCompatActivity implements SearchView.OnQueryTextListener {
     private RecyclerView rview ;
     private Toolbar toolbar;
+    String textSearch = null;
 
 
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
@@ -55,15 +66,103 @@ public class ior_recived extends AppCompatActivity {
         final LinearLayoutManager manager = new LinearLayoutManager(this);
         rview.setLayoutManager(manager);
 
-        String url = "http://"+getApplicationContext().getString(R.string.ip_default)+"/API_IOR/tampil_ior_recived.php";
+        Log.d("ior_recived", "onCreate: "+textSearch);
 
-        StringRequest stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+        String url = "http://"+getApplicationContext().getString(R.string.ip_default)+"/API_IOR/search_ior_recived.php";
+        final String text = textSearch;
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
             @Override
             public void onResponse(String response) {
                 ArrayList<occ> occlist = new JsonConverter<occ>().toArrayList(response, occ.class);
 //                Toast toast = Toast.makeText(getApplicationContext(), kegiatanpemerintahlist.toString(), Toast.LENGTH_LONG);
 //                toast.show();
                 Ior_Recived_Adapter adapter = new Ior_Recived_Adapter(getApplicationContext(), occlist);
+                rview.setLayoutManager(manager);
+                rview.setAdapter(adapter);
+            }
+            }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                if (error instanceof TimeoutError || error instanceof NoConnectionError) {
+                    Toast.makeText(getApplicationContext(),
+                            "Koneksi Timeout , Silahkan Coba Kembali", Toast.LENGTH_SHORT).show();
+                } else if (error instanceof ServerError) {
+                    Toast.makeText(getApplicationContext(),
+                            "Server Mengalami Masalah , Silahkan Coba Kembali", Toast.LENGTH_SHORT).show();
+                } else if (error instanceof NetworkError) {
+                    Toast.makeText(getApplicationContext(),
+                            "Jaringan Mengalami Masalah, Silahkan Coba Kembali", Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(getApplicationContext(),
+                            "Gagal , Silahkan Coba Kembali", Toast.LENGTH_SHORT).show();
+                }
+
+                error.printStackTrace();
+            }
+        })
+//        {
+//            @Override
+//            protected Map<String, String> getParams() throws AuthFailureError {
+//                HashMap<String, String> hashMap = new HashMap<>();
+//                hashMap.put("key", textSearch);
+//                return hashMap;
+//            }
+//
+//
+//        }
+ ;
+        stringRequest.setRetryPolicy(new DefaultRetryPolicy(
+                5000,
+                DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
+                DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+        MySingleton.getInstance(getApplicationContext()).addToRequestQueue(stringRequest);
+
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.searchview, menu);
+        Log.d("cek","AASDASD");
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView = (SearchView) menu.findItem(R.id.search).getActionView();
+        searchView.setSearchableInfo(
+                searchManager.getSearchableInfo(getComponentName()));
+        searchView.setIconifiedByDefault(false);
+        searchView.setOnQueryTextListener(this);
+        return true;
+    }
+
+    @Override
+    public boolean onQueryTextSubmit(String query) {
+        textSearch = query;
+        calldata(query);
+        Log.d("cek ior recived", "onQueryTextSubmit: "+query);
+        return false;
+    }
+
+    @Override
+    public boolean onQueryTextChange(String newText) {
+        textSearch = newText;
+        calldata(newText);
+        Log.d("cek ior recived", "onQueryTextChange: "+newText);
+        return false;
+    }
+
+    private void calldata(final String key){
+        Log.d("cek ior recived", "calldata key: "+key);
+        String url = "http://"+getApplicationContext().getString(R.string.ip_default)+"/API_IOR/search_ior_recived.php";
+        final String text = textSearch;
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                ArrayList<occ> occlist = new JsonConverter<occ>().toArrayList(response, occ.class);
+//                Toast toast = Toast.makeText(getApplicationContext(), kegiatanpemerintahlist.toString(), Toast.LENGTH_LONG);
+//                toast.show();
+                Ior_Recived_Adapter adapter = new Ior_Recived_Adapter(getApplicationContext(), occlist);
+                final LinearLayoutManager manager = new LinearLayoutManager(getApplicationContext());
                 rview.setLayoutManager(manager);
                 rview.setAdapter(adapter);
             }
@@ -86,11 +185,24 @@ public class ior_recived extends AppCompatActivity {
 
                 error.printStackTrace();
             }
-        });
+        })
+        {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                HashMap<String, String> hashMap = new HashMap<>();
+                hashMap.put("key", key);
+                return hashMap;
+            }
 
+
+        }
+                ;
+
+        stringRequest.setRetryPolicy(new DefaultRetryPolicy(
+                5000,
+                DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
+                DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
         MySingleton.getInstance(getApplicationContext()).addToRequestQueue(stringRequest);
-
-
 
     }
 }

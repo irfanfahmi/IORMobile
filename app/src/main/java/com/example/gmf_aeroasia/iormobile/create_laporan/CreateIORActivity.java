cb1706e068af2e990103123ebe4789bc26fb991d
irfanfahmi24 <irfandoombod@gmail.com>
2019-08-18 17:18:01 +0700

    fix upload file & take photo (Create IOR)

--- app/src/main/java/com/example/gmf_aeroasia/iormobile/create_laporan/CreateIORActivity.java
+++ app/src/main/java/com/example/gmf_aeroasia/iormobile/create_laporan/CreateIORActivity.java
@@ -1,6 +1,7 @@
 package com.example.gmf_aeroasia.iormobile.create_laporan;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.app.DatePickerDialog;
 import android.app.ProgressDialog;
 import android.content.Context;
@@ -8,7 +9,6 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.StrictMode;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -59,10 +59,15 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileFilter;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.HashMap;
@@ -151,6 +156,13 @@ public class CreateIORActivity extends AppCompatActivity {
     int selectedRadio;
     Uri uri;
 
+    //TestFile
+    String upLoadServerUri = null;
+    int serverResponseCode = 0;
+    final String uploadFilePath = "/mnt/sdcard/";
+    final String uploadFileName = "service_lifecycle.png";
+    String filepath;
+
     private static final int CAMERA = 1001;
     private static final int GALLERY = 1002;
     @BindView(R.id.tv_name_file)
@@ -164,6 +176,7 @@ public class CreateIORActivity extends AppCompatActivity {
         ButterKnife.bind(this);
 
         cameraPhoto = new CameraPhoto(getApplicationContext());
+        upLoadServerUri = "http://" + getResources().getString(R.string.ip_default) + "/API_IOR/occ/Create.php";
 
         initRealm();
         showButtonBack();
@@ -221,11 +234,17 @@ public class CreateIORActivity extends AppCompatActivity {
                             .load(path)
                             .fitCenter()
                             .into(ivReport);
+                    filepath = path;
+                    Log.d("Cek Path File", "Isinya photophat? "+filepath);
+
                     break;
                 case GALLERY:
                     galleryPhoto.setFileUri(data.getData());
                     uri = data.getData();
                     path = galleryPhoto.getPath();
+                    filepath = path;
+                    Log.d("Cek Path Camera", "Isinya photophat? "+filepath);
+
                     File file = new File(path);
                     if (new ImageFileFilter().accept(file)) {
                         ivReport.setVisibility(View.VISIBLE);
@@ -607,6 +626,8 @@ public class CreateIORActivity extends AppCompatActivity {
             queue = Volley.newRequestQueue(this);
             String url = "http://" + getResources().getString(R.string.ip_default) + "/API_IOR/occ/Create.php";
 
+            uploadFile(filepath);
+
             if (cbHide.isChecked()) {
                 hideReport = "1";
             } else {
@@ -670,6 +691,147 @@ public class CreateIORActivity extends AppCompatActivity {
             queue.add(request);
         }
 //        Log.d(TAG, "sendReport: "+getBase64(uri));
+    }
+
+    @SuppressLint("LongLogTag")
+    public int uploadFile(String sourceFileUri) {
+
+
+        String fileName = sourceFileUri;
+
+        HttpURLConnection conn = null;
+        DataOutputStream dos = null;
+        String lineEnd = "\r\n";
+        String twoHyphens = "--";
+        String boundary = "*****";
+        int bytesRead, bytesAvailable, bufferSize;
+        byte[] buffer;
+        int maxBufferSize = 1 * 1024 * 1024;
+        File sourceFile = new File(sourceFileUri);
+
+        if (!sourceFile.isFile()) {
+
+            //dialog.dismiss();
+
+            Log.e("uploadFile", "Source File not exist :"
+                    +uploadFilePath + "" + uploadFileName);
+
+            runOnUiThread(new Runnable() {
+                public void run() {
+                    Log.e("uploadFile", "Source File not exist :"
+                            +uploadFilePath + "" + uploadFileName);
+
+                }
+            });
+
+            return 0;
+
+        }
+        else
+        {
+            try {
+                Log.d(TAG, "uploadFile: Cek URI to Server "+upLoadServerUri);
+                // open a URL connection to the Servlet
+                FileInputStream fileInputStream = new FileInputStream(sourceFile);
+                URL url = new URL(upLoadServerUri);
+
+                // Open a HTTP  connection to  the URL
+                conn = (HttpURLConnection) url.openConnection();
+                conn.setDoInput(true); // Allow Inputs
+                conn.setDoOutput(true); // Allow Outputs
+                conn.setUseCaches(false); // Don't use a Cached Copy
+                conn.setRequestMethod("POST");
+                conn.setRequestProperty("Connection", "Keep-Alive");
+                conn.setRequestProperty("ENCTYPE", "multipart/form-data");
+                conn.setRequestProperty("Content-Type", "multipart/form-data;boundary=" + boundary);
+                conn.setRequestProperty("uploaded_file", fileName);
+
+                dos = new DataOutputStream(conn.getOutputStream());
+
+                dos.writeBytes(twoHyphens + boundary + lineEnd);
+                // dos.writeBytes("Content-Disposition: form-data; name=uploaded_file; filename="+fileName+"" +lineEnd);
+                dos.writeBytes("Content-Disposition: form-data; name="+fileName+";filename="+ fileName + "" + lineEnd);
+                dos.writeBytes(lineEnd);
+
+                // create a buffer of  maximum size
+                bytesAvailable = fileInputStream.available();
+
+                bufferSize = Math.min(bytesAvailable, maxBufferSize);
+                buffer = new byte[bufferSize];
+
+                // read file and write it into form...
+                bytesRead = fileInputStream.read(buffer, 0, bufferSize);
+
+                while (bytesRead > 0) {
+
+                    dos.write(buffer, 0, bufferSize);
+                    bytesAvailable = fileInputStream.available();
+                    bufferSize = Math.min(bytesAvailable, maxBufferSize);
+                    bytesRead = fileInputStream.read(buffer, 0, bufferSize);
+
+                }
+
+                // send multipart form data necesssary after file data...
+                dos.writeBytes(lineEnd);
+                dos.writeBytes(twoHyphens + boundary + twoHyphens + lineEnd);
+
+                // Responses from the server (code and message)
+                serverResponseCode = conn.getResponseCode();
+                String serverResponseMessage = conn.getResponseMessage();
+
+                Log.i("uploadFile", "HTTP Response is : "
+                        + serverResponseMessage + ": " + serverResponseCode);
+
+                if(serverResponseCode == 200){
+
+                    runOnUiThread(new Runnable() {
+                        public void run() {
+
+//
+//
+//                            Toast.makeText(getApplicationContext(), "File Upload Complete. "+msg,
+//                                    Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                }
+
+                //close the streams //
+                fileInputStream.close();
+                dos.flush();
+                dos.close();
+
+            } catch (MalformedURLException ex) {
+
+//                dialog.dismiss();
+                ex.printStackTrace();
+
+                runOnUiThread(new Runnable() {
+                    public void run() {
+
+                        Toast.makeText(getApplicationContext(), "MalformedURLException",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+                Log.e("Upload file to server", "error: " + ex.getMessage(), ex);
+            } catch (Exception e) {
+
+//                dialog.dismiss();
+                e.printStackTrace();
+
+                runOnUiThread(new Runnable() {
+                    public void run() {
+//
+//                        Toast.makeText(getApplicationContext(), "Got Exception : see logcat ",
+//                                Toast.LENGTH_SHORT).show();
+                    }
+                });
+                Log.e("Upload file to server Exception", "Exception : "+ e.getMessage(), e);
+            }
+//            dialog.dismiss();
+            return serverResponseCode;
+
+        } // End else block
     }
 
 
